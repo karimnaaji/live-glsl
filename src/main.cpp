d38054b8e3b4eed726448dc44bae46d827e5ede7
Karim Naaji <karim.naaji@gmail.com>
2014-10-11 02:06:18 -0400

    glfw window creation

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,33 @@ int main(int argc, char **argv) {
     string absolutePath(s);
     FileWatcher watcher(absolutePath, &callback);
     cout << "start watching " << absolutePath << endl;
-    watcher.startWatching();
+    //watcher.startWatching();
+
+    GLFWwindow* window;
+    int width = 800;
+    int height = 600;
+
+    if(!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(width, height, "GLFW Window", NULL, NULL);
+    if(!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    //initializeOpenGL();
+    //resizeViewport(width, height);
+
+    while(!glfwWindowShouldClose(window)) {
+        //renderFrame();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
