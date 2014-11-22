6a6ae53bd0bf8df6d99bdad462bc29a519f6cc91
Karim Naaji <karim.naaji@gmail.com>
2014-11-21 21:39:00 -0500

    fix tabs

--- src/shader.h
+++ src/shader.h
@@ -5,31 +5,32 @@
 #include <string>
 
 class Shader {
-	public:
-		Shader();
-		~Shader();
+public:
+    Shader();
+    ~Shader();
 
-		const GLuint getProgram() const { return program; };
-    	const GLuint getFragmentShader() const { return fragmentShader; };
-    	const GLuint getVertexShader() const { return vertexShader; };
+    const GLuint getProgram() const { return program; };
+    const GLuint getFragmentShader() const { return fragmentShader; };
+    const GLuint getVertexShader() const { return vertexShader; };
 
-    	void use() const;
-    	bool isInUse() const;
+    void use() const;
+    bool isInUse() const;
 
-    	const GLint getAttribLocation(const std::string& attribute);
-    	bool build(const std::string& fragmentSrc, const std::string& vertexSrc);
-    	
-		void sendUniform(const std::string& name, float x);
-		void sendUniform(const std::string& name, float x, float y);
+    const GLint getAttribLocation(const std::string& attribute);
+    bool build(const std::string& fragmentSrc, const std::string& vertexSrc);
 
-        void detach(GLenum type);
-    private:
-    	GLuint program;
-    	GLuint fragmentShader;
-    	GLuint vertexShader;
-		
-		GLuint link();
-		GLuint compileShader(const std::string& src, GLenum type);
-		GLint getUniformLocation(const std::string& uniformName) const;
-        void printInfoLog(GLuint shader);
+    void sendUniform(const std::string& name, float x);
+    void sendUniform(const std::string& name, float x, float y);
+
+    void detach(GLenum type);
+    
+private:
+    GLuint program;
+    GLuint fragmentShader;
+    GLuint vertexShader;
+
+    GLuint link();
+    GLuint compileShader(const std::string& src, GLenum type);
+    GLint getUniformLocation(const std::string& uniformName) const;
+    void printInfoLog(GLuint shader);
 };
